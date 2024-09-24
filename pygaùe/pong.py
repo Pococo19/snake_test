1e9017c0934a69c16e864400f18328ccb04bae77
Pococo19 <pococo@STAGEINFO.DOM.PAS>
2024-09-24 09:21:54 +0200

    add pygame

---
+++ pygaùe/pong.py
@@ -0,0 +1,108 @@
+import pygame, sys, random
+
+pygame.init()
+
+screen_width = 1280
+screen_height = 720
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("PONG!")
+
+clock = pygame.time.Clock()
+
+ball = pygame.Rect(0,0,30,30)
+ball.center = (screen_width / 2, screen_height / 2)
+cpu = pygame.Rect(0,0,20,100)
+cpu.centery = screen_height/2
+
+cpu_points = 0
+player_points = 0
+
+score_font = pygame.font.Font(None, 100)
+
+ball_speed_x = 6
+ball_speed_y = 6
+player_speed = 0
+cpu_speed = 0;
+
+player = pygame.Rect(0,0,20,100)
+player.midright = (screen_width, screen_height / 2)
+
+def reset_ball():
+    global ball_speed_x, ball_speed_y
+
+    ball.x = screen_width / 2- 10
+    ball.y = random.randint(10, 100)
+    ball_speed_x *= random.choice([-1, 1])
+    ball_speed_y *= random.choice([-1, 1])
+
+def point_won(winner):
+    global player_points, cpu_points
+
+    if winner == "player":
+        player_points += 1
+    else:
+        cpu_points += 1
+    reset_ball()
+    
+def move_player(event):
+    global player
+
+    if event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_UP:
+                player.y -= 6
+    if event.type == pygame.KEYUP:
+        if event.type == pygame.K_DOWN:
+                player.y += 6
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit() 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player_speed = -6
+            if event.key == pygame.K_DOWN:
+                player_speed = 6
+        else:
+            player_speed = 0
+
+
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+    player.y += player_speed
+    cpu.y += cpu_speed
+
+    if ball.centery <= cpu.centery:
+        cpu_speed = -6
+    else:
+        cpu_speed = 6
+    if ball.bottom >= screen_height or ball.top <= 0:
+        ball_speed_y *= -1
+    if ball.colliderect(player) or ball.colliderect(cpu):
+        ball_speed_x *= -1
+    if cpu.top <= 0:
+        cpu.top = 0
+    if cpu.bottom >= screen_height:
+        cpu.bottom = screen_height
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+    if ball.right >= screen_width:
+        point_won("cpu")
+    if ball.left <= 0:
+        point_won("player")
+
+    screen.fill('black')
+    cpu_score = score_font.render(str(cpu_points), True, "white")
+    player_score = score_font.render(str(player_points), True, "white")
+    screen.blit(cpu_score, (screen_width / 4,20))
+    screen.blit(player_score, (3 * screen_width / 4,20))
+    pygame.draw.aaline(screen, 'white', (screen_width/2, 0), (screen_width/2, screen_height))
+    pygame.draw.ellipse(screen, 'white', ball)
+    pygame.draw.rect(screen, 'white', cpu)
+    pygame.draw.rect(screen, 'white', player)
+    pygame.display.update()
+    clock.tick(60)
